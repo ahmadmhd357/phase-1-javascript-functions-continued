bf6438d3418af6a1498a6b2c0fa3735a4dfa2aa5
Ahmad Hariri <ahmadmhd357@gmail.com>
2022-10-10 03:17:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+};
+const mondayWork =  function (activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+function wrapAdjective(value1="*"){
+    const adjective= function(value2="special"){
+        return `You are ${value1}${value2}${value1}!`;
+    };
+    return adjective;
+};
